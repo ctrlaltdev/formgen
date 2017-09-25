ac93f71be0dcc6138e76c19938a00e53746729f3
implose <void@implo.se>
2017-09-25 19:55:52 +0200

    WIP: Basic structure

---
+++ routes/send.js
@@ -0,0 +1,11 @@
+'use strict';
+var conf = require('../conf');
+var express = require('express');
+var router = express.Router();
+
+// GET: /
+router.post('/', function(req, res) {
+
+});
+
+module.exports = router;
